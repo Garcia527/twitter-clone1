266bf76ccbaaf8410194d85493d5017fc2f85dde
Oscar Garcia <ogarcia@uwm.edu>
2020-09-05 09:49:46 -0500

    Created Sidebar classes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Sidebar from './Sidebar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    // following the bem convention
+    <div className="app">
       <h1> Twitter clone</h1>
+
+      { /* Sidebar */ }
+      <Sidebar />
+
+      { /* Feed */ }
+
+      { /* Widgets */ }
+
     </div>
   );
 }
