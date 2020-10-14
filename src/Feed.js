b501b8cfc156d64ce6838f8ffac14ea17d9268e3
Oscar Garcia <ogarcia@uwm.edu>
2020-10-14 16:36:23 -0500

    init

--- src/Feed.js
+++ src/Feed.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import "./Feed.css";
 
 function Feed() {
     return (
         <div>
             {/* Header */}
+            <div clasName="feed">
             <h2>Home</h2>
+            </div>
 
             {/* Tweetbox */}
 
